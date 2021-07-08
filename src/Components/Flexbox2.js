3b065fbaf1ecff79217409aaa773a90514403783
Niketan <niketanjha@gmail.com>
2021-07-08 14:08:02 +0530

    flexbox1,2 and 3

--- src/Components/Flexbox2.js
+++ src/Components/Flexbox2.js
@@ -5,24 +5,30 @@ class Flexbox2 extends React.Component{
   render(){
     return(
       <div className="mainClass2">
-        <div className="Nav2">
+        <div className="column1">
+          <div className="Nav1">  
             <span className="textClass2" >Nav</span>
-          </div>
-          
-        <div className="headerClass2">
-          <span className="textClass2">Header2</span>
+          </div> 
         </div>
-        <div className="middleClass2">
-          <div className="articleClass2">
-            <span className="textClass2">Article</span>
+        <div className="column2">
+          <div className="headerClass1">
+            <span className="textClass2">Header2</span>
           </div>
-          <div className="asideClass2">
-            <span className="textClass2">Aside</span>
+          <div className="downDiv">
+            <div className="div1">
+              <div className="articleClass2">
+                <span className="textClass2">Article</span>
+              </div>
+              <div className="footerClass2">
+                <span className="textClass2">Footer</span>
+              </div>
+            </div>
+            <div className="div2">
+              <div className="asideClass2">
+                <span className="textClass2">Aside</span>
+              </div>
+            </div>
           </div>
-        </div>
-        
-        <div className="footerClass2">
-          <span className="textClass2">Footer</span>
         </div>
       </div>
     );
